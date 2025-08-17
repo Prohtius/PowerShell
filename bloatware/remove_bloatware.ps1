f613966abf93a22d3fd2224a9d2692cabff197e0
Steve Grimstead <55750267+Prohtius@users.noreply.github.com>
2025-08-17 16:36:35 -0500

    Create remove_bloatware.ps1

---
+++ bloatware/remove_bloatware.ps1
@@ -0,0 +1,31 @@
+$bloatapps = @(
+"*WindowsMaps",
+"MSTeams",
+"*YourPhone",
+"*Zune*",
+"*WindowsMaps",
+"*WindowsCamera",
+"*WindowsAlarms",
+"*Todos",
+"*People",
+"*MicrosoftOfficeHub",
+"*MicrosoftSolitaireCollection",
+"*Bing*",
+"*windowscommunicationsapps",
+"*Clipchamp",
+"*OutlookForWindows",
+"*Windows.Photos",
+"*WindowsSoundRecorder",
+"*MicrosoftStickyNotes",
+"*Copilot",
+"*WindowsFeedbackHub",
+"Microsoft.StartExperiencesApp",
+"*Widget*",
+"*family*"
+)
+
+foreach ($bloatapp in $bloatapps)
+{
+Get-AppxProvisionedPackage -Online | Where "DisplayName" -like $bloatapp | Remove-AppxProvisionedPackage -Online -Verbose
+Get-AppxPackage -Name $bloatapp -AllUsers | Remove-AppxPackage -AllUsers -Verbose
+}
