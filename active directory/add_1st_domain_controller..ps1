79cab9f4ea19fd5827e5d684f2057dba923a8324
Steve Grimstead <55750267+Prohtius@users.noreply.github.com>
2025-08-19 11:13:05 -0500

    :tada: :wrench: :package: Create add_1st_domain_controller..ps1

---
+++ active directory/add_1st_domain_controller..ps1
@@ -0,0 +1,36 @@
+```powershell
+<#
+  Author: Steve Grimstead (Prohtius)
+  Date: 08-19-2025
+  Version: 1.0  
+  Purpose: Creates the first domain controller in a forest using the
+    domain name provided.
+
+  Link: 
+
+  *****************************
+  *** Only for new forests! ***
+  *****************************
+#>
+
+#  change the domain_name variable to the domain name of your domain!
+$domain_name = ""
+
+<#
+  Gets the first part of the domain name and uses it as the
+  NetBIOS name, if this is a sub domain such as lan.example.org,
+  then the NetBIOS name will be LAN.
+#>
+$net_bios_name = $domain_name.Split(".")[0]
+
+$forest_settings = @{
+    DomainName        = $domain_name
+    DomainNetBIOSName = $net_bios_name
+    DomainMode        = 6
+    ForestMode        = 6
+    InstallDNS        = $true
+    Force             = $true
+}
+
+Install-ADDSForest @forest_settings
+```
